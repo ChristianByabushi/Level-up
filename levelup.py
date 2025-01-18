28f63a0f28011b85083041f0a640a1fc9678bfe1
christianByabushi <murchristian@gmail.com>
2025-01-18 18:05:56 +0200

    add comments to break statement

--- levelup.py
+++ levelup.py
@@ -10,7 +10,7 @@ class Levelup:
         for number in range(2,nb):
             for factor in range(2,int(math.sqrt(number))+1): 
                 if number % factor == 0:
-                    break ## we stop the for loop
+                    break ## we stop the for-loop
             else: 
                 allprimesnumbers.append(number)
         return allprimesnumbers
